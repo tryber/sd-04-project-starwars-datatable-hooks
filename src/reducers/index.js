70570ba1b8a2b7356f13c626445167cbbc23ad9a
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-07-25 19:17:23 -0300

    remove src

--- src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import planetReducer from './planetReducer';
-import filters from './FilterReducer';
-
-
-const rootReducer = combineReducers({ planetReducer, filters });
-
-export default rootReducer;
