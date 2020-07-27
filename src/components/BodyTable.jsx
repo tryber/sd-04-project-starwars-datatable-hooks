3aedc48c88d55450be1c7c3d30c128a8eb878cc1
TerezaCarvalho <Familia@MacBook-Air-de-FamiliaCarvalhoAugusto.local>
2020-07-27 19:16:11 -0300

    cc

--- src/components/BodyTable.jsx
+++ src/components/BodyTable.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const BodyTable = ({ planet }) => {
+function BodyTable({ planet }) {
   return (
     <tr>
-    <td>{planet.name}</td>
-    <td>{planet.rotation_period}</td>
-    <td>{planet.orbital_period}</td>
-    <td>{planet.diameter}</td>
-    <td>{planet.climate}</td>
-    <td>{planet.gravity}</td>
-    <td>{planet.terrain}</td>
-    <td>{planet.surface_water}</td>
-    <td>{planet.population}</td>
-    <td>{planet.films}</td>
-    <td>{planet.created}</td>
-    <td>{planet.edited}</td>
-    <td>{planet.url}</td>
-  </tr>
-  )
-};
+      <td>{planet.name}</td>
+      <td>{planet.rotation_period}</td>
+      <td>{planet.orbital_period}</td>
+      <td>{planet.diameter}</td>
+      <td>{planet.climate}</td>
+      <td>{planet.gravity}</td>
+      <td>{planet.terrain}</td>
+      <td>{planet.surface_water}</td>
+      <td>{planet.population}</td>
+      <td>{planet.films}</td>
+      <td>{planet.created}</td>
+      <td>{planet.edited}</td>
+      <td>{planet.url}</td>
+    </tr>
+  );
+}
 
 export default BodyTable;
 
