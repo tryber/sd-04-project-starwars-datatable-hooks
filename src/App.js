6669c0bbcc2325d79ab01bb46b4f315c6955d76a
deboracosilveira <deboracosilveira@gmail.com>
2020-07-27 15:44:31 -0300

    App first changes

--- src/App.js
+++ src/App.js
@@ -1,42 +1,56 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 
 import { fetchPlanetsAPI } from './actions';
 import './App.css';
+
+import StarWarsContext from './context/StarWarsContext';
 
 import SearchPlanet from './components/SearchPlanet';
 import ComparisonFilter from './components/ComparisonFilter';
 import Filters from './components/Filters';
 import Table from './components/Table';
 import OrderFilter from './components/OrderFilter';
+import getPlanets from './services/planetsAPI';
 
-class App extends Component {
-  componentDidMount() {
-    const { getPlanets } = this.props;
-    getPlanets();
-  }
-  render() {
-    return (
+const App = () => {
+  // componentDidMount() {
+  //   const { getPlanets } = this.props;
+  //   getPlanets();
+  // }
+
+  const [isLoading, setIsLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    // Deve funcionar como componentDidMount para chamar os planetas
+    setData(getPlanets());
+    // setIsLoading(true)
+  }, []);
+
+  return (
+    <StarWarsContext.Provider /* value={} */>
       <div className="App">
-        <header className="App-header">
+      debora
+        {/* <header className="App-header">
           <SearchPlanet />
           <OrderFilter />
           <ComparisonFilter />
           <Filters />
         </header>
-        <Table />
+        <Table /> */}
       </div>
-    );
-  }
-}
-
-const mapDispatchToProps = (dispatch) => ({
-  getPlanets: () => dispatch(fetchPlanetsAPI()),
-});
-
-export default connect(null, mapDispatchToProps)(App);
-
-App.propTypes = {
-  getPlanets: PropTypes.func.isRequired,
+    </StarWarsContext.Provider>
+  );
 };
+
+// const mapDispatchToProps = (dispatch) => ({
+//   getPlanets: () => dispatch(fetchPlanetsAPI()),
+// });
+
+// export default connect(null, mapDispatchToProps)(App);
+export default App;
+
+// App.propTypes = {
+//   getPlanets: PropTypes.func.isRequired,
+// };
