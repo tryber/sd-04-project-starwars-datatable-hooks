30b1988f9129d8dc29c03351b1fd3fec25c78e92
Fabiola Deiro <fabioladeiro7@gmail.com>
2020-07-27 18:59:19 -0300

    CC

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useContext, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import StartWarsContext from './context/StarWarsContext';
 import Table from './components/Table';
