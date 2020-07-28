89144e2f0e21db6fd88fbbbd4fabe40eabcc83bc
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-07-28 17:58:26 -0300

    funciona mas o cc n ajuda

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,30 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import TextFilter from './searchBarComponents/TextFilter';
 // import SortFilter from './searchBarComponents/SortFilter';
 import NumericFilter from './searchBarComponents/NumericFilter';
+import StarWarsContext from '../context/StarWarsContext';
 
 function SearchBar() {
+  const { filters } = useContext(StarWarsContext)
+  const NumericFilters = filters.filterByNumericValues;
+
   return (
     <div className="searchBar">
       <TextFilter />
       <NumericFilter />
       <div>
         <div><h1>Filtros:</h1></div>
-        {/* <div>
-          {filters.map((filter) => (
+        <div>
+          {NumericFilters.map((filter) => (
             <div data-testid="filter" className="filters">
               <p key={filter.value}>
                 {filter.column} {filter.comparison} {filter.value}
               </p>
-              <button
+              {/* <button
                 type="button"
                 onClick={() => this.props.deleteFilter(filter.column)}
               >
                 X
-              </button>
+              </button> */}
             </div>
           ))}
-          </div> */}
+          </div>
       </div>
     </div>
   );
