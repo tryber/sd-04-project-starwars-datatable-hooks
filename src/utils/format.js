53d3071152d12405c8df5d1bb2059e722afb82e5
procopio420 <lucas-procopio@outlook.com>
2020-07-28 14:35:48 -0300

    ok - Hooks & Context

--- src/utils/format.js
+++ src/utils/format.js
@@ -1,11 +1,9 @@
 export function removeUnderline(str) {
-  const noUnderline = str.replace('_', ' ');
-  return noUnderline;
+  return str.replace('_', ' ');
 }
 
 export function capitalize(str) {
   const arr = str.split(' ');
-  const capitalizedArr = arr.map(strArr => strArr.charAt(0).toUpperCase() + strArr.slice(1));
-  const capitalized = capitalizedArr.join(' ');
-  return capitalized;
+  const capitalizedArr = arr.map((strArr) => strArr.charAt(0).toUpperCase() + strArr.slice(1));
+  return capitalizedArr.join(' ');
 }
