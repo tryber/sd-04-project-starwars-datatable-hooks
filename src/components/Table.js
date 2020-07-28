189b2bcb21a541c9fc9dfd33994c7f3408623f70
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-07-28 16:31:51 -0300

    filtrando, pausa para alegrar o cc

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,18 +3,26 @@ import StarWarsContext from '../context/StarWarsContext';
 
 
 function Table() {
-  const { dataApi, isLoading } = useContext(StarWarsContext);
+  const { dataApi, isLoading, filterByName } = useContext(StarWarsContext);
 
   const filteredData = () => {
-    return dataApi ;
+    let planets = [...dataApi];
+    if (filterByName.name.length > 0) {
+      planets = planets.filter((planet) => planet.name.includes(filterByName.name));
+    }
+    return planets;
   }
+
   const chaves =
     (dataApi.length !== 0) ? Object.keys(dataApi[0]).filter((keys) => keys !== 'residents') : [];
+
   const planets = filteredData();
+
   if (isLoading) return <h1>Loading...</h1>
   return (
     <div>
       <table className="table">
+        {console.log(filterByName)}
         <thead>
           <tr>
             {chaves.map((chave) => (<th key={chave}>{chave}</th>))}
