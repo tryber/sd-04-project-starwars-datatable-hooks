22e1d3d69e58537344c227dc56ab9b855630678d
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-07-28 16:52:55 -0300

    libera noi cc

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -1,12 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import StarWarsContext from './StarWarsContext';
 import PropTypes from 'prop-types';
+import StarWarsContext from './StarWarsContext';
 
 
 const StarWarsProvider = ({ children }) => {
   const [dataApi, setDataApi] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [filterByName, setfilterByName] = useState({ name: '' });
+  const [column, setColumn] = useState('');
+  const [comparison, setComparison] = useState('');
+  const [value, setValue] = useState('');
 
   useEffect(() => {
     (async () => {
@@ -15,15 +18,25 @@ const StarWarsProvider = ({ children }) => {
         const { results } = await request.json();
         await setDataApi(results);
         await setIsLoading(false);
-      } catch (error) { console.log('Deu ruim',error) };
+      } catch (error) { console.log('Deu ruim', error); }
     })();
   }, []);
 
   const states = {
     dataApi,
     isLoading,
-    filterByName,
+    filters: {
+      filterByName,
+      filterByNumericValues: {
+        column,
+        comparison,
+        value,
+      },
+    },
     setfilterByName,
+    setColumn,
+    setComparison,
+    setValue,
   };
 
   return (
@@ -39,5 +52,5 @@ StarWarsProvider.propTypes = {
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.element),
     PropTypes.element,
-    ]).isRequired, 
+  ]).isRequired,
 }
