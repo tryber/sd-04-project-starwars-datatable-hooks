104fb4134009ef2d101066fce4160dcec87a4e76
jessica-tavares <jessicatavaraeseel@gmail.com>
2020-07-28 18:57:27 -0300

    agora foi 100%, so o cc libera

--- src/components/Table.js
+++ src/components/Table.js
@@ -23,9 +23,10 @@ function Table() {
         <tbody>
           {planets.map((planet) => (
             <tr key={`${planet.name} 1`}>
-              {chaves.map((chave) => (
-                <td key={`${planet.name} ${chave}`}>{planet[chave]}</td>
-              ))}
+              {chaves.map((chave) => {
+                if (chave !== 'name') return <td key={`${planet.name} ${chave}`}>{planet[chave]}</td>
+                return <td key={`${planet.name} ${chave}`} data-testid="planet-name">{planet[chave]}</td>
+              })}
             </tr>
           ))}
         </tbody>
