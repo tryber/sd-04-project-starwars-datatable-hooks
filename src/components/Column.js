9aa9eaaa7a1e8711fcf0bf0429e906801120500f
AliceAAtalla <aliceatalla36@gmail.com>
2020-07-28 10:28:27 -0300

    Requirement 4

--- src/components/Column.js
+++ src/components/Column.js
@@ -3,14 +3,19 @@ import StarWarsContext from '../context/StarWarsContext';
 
 const Column = () => {
   const states = useContext(StarWarsContext);
-  const { selectedFilters } = states;
-  const column = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const { selectedFilters, filterByNumericValues } = states;
+  const columns = ['population', 'orbital_period', 'diameter', 'rotation_period', 'surface_water'];
+  const arrayColumn = () => {
+    if (filterByNumericValues.length === 0) return columns;
+    const valuesUsed = filterByNumericValues.map(({ column }) => column);
+    return columns.filter((value) => !valuesUsed.includes(value));
+  };
 
   return (
     <div>
       <select onChange={selectedFilters} name="column" data-testid="column-filter" defaultValue="default">
         <option value="default" disabled>Choose a Column</option>
-        {column.map((op) => <option key={op} value={op}>{op}</option>)}
+        {arrayColumn().map((op) => <option key={op} value={op}>{op}</option>)}
       </select>
     </div>
   );
