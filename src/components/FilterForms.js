652cf8f15fc514fe3331fd389a3d8887c5248bad
Thabata <dornelasthabata@gmail.com>
2020-07-29 16:50:39 -0300

    Tirando o redux do FilterForms

--- src/components/FilterForms.js
+++ src/components/FilterForms.js
@@ -1,36 +1,19 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
+import React, { useContext, useState } from 'react';
 import ShowFilters from './ShowFilters';
-import { saveFilterData } from '../actions';
+import { StarWarsContext } from '../context/StarWarsContext';
 
-class FilterForms extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      column: '',
-      comparison: '',
-      number: '',
-    };
-    this.filterOptions = this.filterOptions.bind(this);
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+function FilterForms() {
+  const { submitFilterData, filterByNumericValues, options } = useContext(StarWarsContext);
+  const [localState, setLocalState] = useState({ column: '', comparison: '', number: '' });
+  function handleChange({ name, value }) {
+    setLocalState({ ...localState, [name]: value });
   }
-
-  handleChange(event) {
-    const { value, name } = event.target;
-    this.setState({ [name]: value });
-  }
-
-  handleSubmit(event) {
+  function handleSubmit(event) {
     event.preventDefault();
-    const { submitFilterData } = this.props;
-    const { column, comparison, number } = this.state;
+    const { column, comparison, number } = localState;
     submitFilterData(column, comparison, number);
   }
-
-  filterOptions() {
-    const { filterByNumericValues, options } = this.props;
+  function filterOptions() {
     let newOptions = [...options];
     if (filterByNumericValues.length >= 1) {
       filterByNumericValues.forEach(({ column }) => {
@@ -39,61 +22,31 @@ class FilterForms extends React.Component {
     }
     return newOptions;
   }
-
-  renderSelect(options, testId, name) {
+  function renderSelect(newOptions, testId, name) {
     return (
-      <select onChange={(e) => this.handleChange(e)} data-testid={testId} name={name}>
+      <select onChange={(e) => handleChange(e.target)} data-testid={testId} name={name}>
         <option defaultChecked>{name}</option>
-        {options.map((option) => (
-          <option key={option} value={option}>
-            {option}
-          </option>
-        ))}
+        {newOptions.map((option) => (<option key={option} value={option}>{option}</option>))}
       </select>
     );
   }
-
-  render() {
-    const newOptions = this.filterOptions();
-    return (
-      <div>
-        <form onSubmit={(e) => this.handleSubmit(e)}>
-          {this.renderSelect(newOptions, 'column-filter', 'column')}
-          {this.renderSelect(
-            ['maior que', 'menor que', 'igual a'],
-            'comparison-filter',
-            'comparison',
-          )}
-          <input
-            onChange={(e) => this.handleChange(e)}
-            data-testid="value-filter"
-            type="number"
-            name="number"
-          />
-          <button type="submit" data-testid="button-filter">
-            acionar filtro
-          </button>
-        </form>
-        <ShowFilters />
-      </div>
-    );
-  }
+  const newOptions = filterOptions();
+  return (
+    <div>
+      <form onSubmit={(e) => handleSubmit(e)}>
+        {renderSelect(newOptions, 'column-filter', 'column')}
+        {renderSelect(['maior que', 'menor que', 'igual a'], 'comparison-filter', 'comparison')}
+        <input
+          onChange={(e) => handleChange(e.target)}
+          data-testid="value-filter"
+          type="number"
+          name="number"
+        />
+        <button type="submit" data-testid="button-filter">acionar filtro</button>
+      </form>
+      <ShowFilters />
+    </div>
+  );
 }
 
-const mapStateToProps = (state) => ({
-  filterByNumericValues: state.filters.filterByNumericValues,
-  options: state.filters.options,
-});
-
-const mapDispatchToProps = (dispatch) => ({
-  submitFilterData: (column, comparison, number) =>
-    dispatch(saveFilterData(column, comparison, number)),
-});
-
-FilterForms.propTypes = {
-  submitFilterData: PropTypes.func.isRequired,
-  filterByNumericValues: PropTypes.arrayOf(PropTypes.object).isRequired,
-  options: PropTypes.arrayOf(PropTypes.string).isRequired,
-};
-
-export default connect(mapStateToProps, mapDispatchToProps)(FilterForms);
+export default FilterForms;
