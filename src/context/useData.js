41273804a1d3f82529f80d937cbb556707724056
Thabata <dornelasthabata@gmail.com>
2020-07-29 12:13:18 -0300

    Esqueleto do useData

---
+++ src/context/useData.js
@@ -0,0 +1,9 @@
+import { useContext, useEffect } from 'react';
+import { StarWarsContext } from './StarWarsContext';
+import getPlanets from '../services/api';
+
+// const useData = () => {
+//   const tal = useContext(StarWarsContext);
+
+
+export default useData;
