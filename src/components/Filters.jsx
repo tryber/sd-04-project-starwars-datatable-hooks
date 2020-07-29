37d1d9afe83428f9108f37bf0d8ee8e1f35c2911
matheusysd <matheusyurisd@gmail.com>
2020-07-29 20:01:03 -0300

    Requisito 3 pt2

--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -22,9 +22,9 @@ const Filters = () => {
         <label htmlFor="comparison">
           <select name="comparison" data-testid="comparison-filter">
             <option defaultValue>Comparison</option>
-            <option value="maior que">Maior que</option>
-            <option value="menor que">Menor que</option>
-            <option value="igual a">Igual a</option>
+            <option value="maior que">maior que</option>
+            <option value="menor que">menor que</option>
+            <option value="igual a">igual a</option>
           </select>
         </label>
         <input name="number" type="number" data-testid="value-filter" />
