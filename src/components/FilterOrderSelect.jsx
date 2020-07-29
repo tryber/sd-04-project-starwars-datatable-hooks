e622c2499b284f7d7771cb5aa8250c6d66057e36
DanielLMiranda <danieldelacerdamiranda@gmail.com>
2020-07-29 17:54:51 -0300

    fix/codeclimate

--- src/components/FilterOrderSelect.jsx
+++ src/components/FilterOrderSelect.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const FilterOrderSelect = ({ column, setColumn, tableHeaders }) => {
-  return (
-    <select
-      name="column"
-      id="column"
-      value={column}
-      onChange={(event) => setColumn(event.target.value)}
-      data-testid="column-sort"
-    >
-      {tableHeaders.map((item) => (
-        <option key={item} value={item.charAt(0).toUpperCase() + item.slice(1)}>
-          {item.charAt(0).toUpperCase() + item.slice(1)}
-        </option>
-      ))}
-    </select>
-  );
-};
+const FilterOrderSelect = ({ column, setColumn, tableHeaders }) => (
+  <select
+    name="column"
+    id="column"
+    value={column}
+    onChange={(event) => setColumn(event.target.value)}
+    data-testid="column-sort"
+  >
+    {tableHeaders.map((item) => (
+      <option key={item} value={item.charAt(0).toUpperCase() + item.slice(1)}>
+        {item.charAt(0).toUpperCase() + item.slice(1)}
+      </option>
+    ))}
+  </select>
+);
 
 FilterOrderSelect.propTypes = {
   column: PropTypes.string.isRequired,
