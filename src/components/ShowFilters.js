d427253025fda13d54636d7121679480ddfdfafc
Thabata <dornelasthabata@gmail.com>
2020-07-29 17:49:24 -0300

    Tentando usar o useData

--- src/components/ShowFilters.js
+++ src/components/ShowFilters.js
@@ -1,40 +1,26 @@
-// import React from 'react';
-// import { connect } from 'react-redux';
-// import PropTypes from 'prop-types';
-// import { remove } from '../actions';
+import React, { useContext } from 'react';
+import { StarWarsContext } from '../context/StarWarsContext';
 
-// const ShowFilters = ({ filterByNumericValues, removeFilter }) => {
-//   if (filterByNumericValues.length >= 1) {
-//     return (
-//       <div>
-//         Filters
-//         <ul>
-//           {filterByNumericValues.map(({ column, comparison, value }) => (
-//             <li key={column} data-testid="filter">
-//               {`${column} ${comparison} ${value} `}
-//               <button type="button" onClick={() => removeFilter(column)}>
-//                 X
-//               </button>
-//             </li>
-//           ))}
-//         </ul>
-//       </div>
-//     );
-//   }
-//   return <div />;
-// };
+const ShowFilters = () => {
+  const { filterByNumericValues, removeFilter } = useContext(StarWarsContext);
+  if (filterByNumericValues.length >= 1) {
+    return (
+      <div>
+        Filters
+        <ul>
+          {filterByNumericValues.map(({ column, comparison, value }) => (
+            <li key={column} data-testid="filter">
+              {`${column} ${comparison} ${value} `}
+              <button type="button" onClick={() => removeFilter(column)}>
+                X
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+  return <div />;
+};
 
-// const mapStateToProps = (state) => ({
-//   filterByNumericValues: state.filters.filterByNumericValues,
-// });
-
-// const mapDispatchToProps = (dispatch) => ({
-//   removeFilter: (column) => dispatch(remove(column)),
-// });
-
-// ShowFilters.propTypes = {
-//   filterByNumericValues: PropTypes.arrayOf(PropTypes.object).isRequired,
-//   removeFilter: PropTypes.func.isRequired,
-// };
-
-// export default connect(mapStateToProps, mapDispatchToProps)(ShowFilters);
+export default ShowFilters;
--- src/context/useData.js
+++ src/context/useData.js
@@ -1,23 +1,25 @@
-// Refatorar
+import { useContext, useEffect } from 'react';
+import { StarWarsContext } from './StarWarsContext';
+import getPlanets from '../services/planetsAPI';
 
-// const useData = () => {
-//   const { setData, setIsLoading, setError, isLoading, data } = useContext(StarWarsContext);
+const useData = () => {
+  const { setData, setIsLoading, setError, isLoading, data } = useContext(StarWarsContext);
 
-//   useEffect(() => {
-//     setIsLoading(true);
-//     getPlanets().then(
-//       (json) => {
-//         setData(json.results);
-//         setIsLoading(false);
-//       },
-//       (error) => {
-//         setError(error);
-//         setIsLoading(false);
-//       },
-//     );
-//   }, [setData, setIsLoading, setError]);
+  useEffect(() => {
+    setIsLoading(true);
+    getPlanets().then(
+      (json) => {
+        setData(json.results);
+        setIsLoading(false);
+      },
+      (error) => {
+        setError(error);
+        setIsLoading(false);
+      },
+    );
+  }, [setData, setIsLoading, setError]);
 
-//   return { data, isLoading };
-// };
+  return { data, isLoading };
+};
 
-// export default useData;
+export default useData;
