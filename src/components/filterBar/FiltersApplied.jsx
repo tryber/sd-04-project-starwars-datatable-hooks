7d409507dda4360c5d046b83dcaf4abdfd9a7ce5
henrique <hbaetaleite@gmail.com>
2020-07-29 16:37:14 -0300

    codeclimate v2

--- src/components/filterBar/FiltersApplied.jsx
+++ src/components/filterBar/FiltersApplied.jsx
@@ -8,18 +8,16 @@ const FiltersApplied = () => {
   return (
     <div>
       {filters.length > 0 ? <h3>Filters Applied: </h3> : null}
-      {filters.length > 0
-        ? filters.map(({ column, comparison, value }) => (
-            <div data-testid="filter" key={column}>
-              <p>Column: {column}</p>
-              <p>Comparison: {comparison}</p>
-              <p>Value: {value}</p>
-              <button onClick={() => removeFilter(column)} type="button">
-                remove filter
-              </button>
-            </div>
-          ))
-        : null}
+      {filters.map(({ column, comparison, value }) => (
+        <div data-testid="filter" key={column}>
+          <p>Column: {column}</p>
+          <p>Comparison: {comparison}</p>
+          <p>Value: {value}</p>
+          <button onClick={() => removeFilter(column)} type="button">
+            remove filter
+          </button>
+        </div>
+      ))}
     </div>
   );
 };
