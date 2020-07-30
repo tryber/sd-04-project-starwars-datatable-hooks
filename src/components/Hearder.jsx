f6e21186f015d807519faf334f42c57005ed5d76
djonata-dias <diasdjonata@gmail.com>
2020-07-29 21:41:54 -0300

    Criei o arquivo Hearder com o nome dos dados no começo da tabela

---
+++ src/components/Hearder.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Hearder = () =>
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Population</th>
+      <th>Rotation period</th>
+      <th>Orbital period</th>
+      <th>Diameter</th>
+      <th>Climate</th>
+      <th>Gravity</th>
+      <th>Terrain</th>
+      <th>Surface water</th>
+      <th>Created</th>
+      <th>Edited</th>
+    </tr>
+  </thead>;
+
+
+export default Hearder;
