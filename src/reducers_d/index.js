eec62a67b4412396147eca271618b54636052b53
danilossousa <danilosantossousa@gmail.com>
2020-07-30 13:23:14 -0300

    v0.1.0.6

--- src/reducers_d/index.js
+++
@@ -1,7 +0,0 @@
-// import { combineReducers } from 'redux';
-// import data from './reducerData';
-// import filters from './reducerFilters';
-
-// const rootReducer = combineReducers({ data, filters });
-
-// export default rootReducer;
