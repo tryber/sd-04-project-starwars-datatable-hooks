7afed0cf89100b9fd8a33418ce73d1a98e101ac1
djonata-dias <diasdjonata@gmail.com>
2020-07-29 21:41:02 -0300

    Coloquei o app como children do store(Provider) no arquivo index

--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Store } from './utils/store';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Store>
+    <App />
+  </Store>,
+  document.getElementById('root'),
+);
