319fc4d5592572556d0ce80ae28bc874aaff5528
djonata-dias <diasdjonata@gmail.com>
2020-07-29 21:47:57 -0300

    CC validation

--- src/App.js
+++ src/App.js
@@ -3,15 +3,10 @@ import './App.css';
 import Table from './components/Table';
 import Filters from './components/Filters';
 
-const App = () => {
-  return (
-    <div >
-      <Filters />
-      <Table />
-    </div>
-  )
-
-}
-
+const App = () =>
+  <div >
+    <Filters />
+    <Table />
+  </div>;
 
 export default App;
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -2,40 +2,31 @@ import React, { useContext } from 'react';
 import StoreProvider from '../utils/store';
 import Header from './Hearder';
 
-
-
-
-
 const Table = () => {
   const { planets, filters } = useContext(StoreProvider);
   console.log(filters.filterByName);
   const newPlanets = planets.filter(({ name }) =>
-    (name.toLowerCase()).includes(filters.filterByName)
-  )
-
-
-
+    (name.toLowerCase()).includes(filters.filterByName),
+  );
   return (
     <table>
-      <Header />
-      <tbody>
-        {newPlanets.map((planet) => {
-          return (
-            <tr key={planet.name}>
-              <td>{planet.name}</td>
-              <td>{planet.population}</td>
-              <td>{planet.rotation_period}</td>
-              <td>{planet.orbital_period}</td>
-              <td>{planet.diameter}</td>
-              <td>{planet.climate}</td>
-              <td>{planet.gravity}</td>
-              <td>{planet.terrain}</td>
-              <td>{planet.surface_water}</td>
-              <td>{planet.created}</td>
-              <td>{planet.edited}</td>
-            </tr>
-          )
-        })}
+      <Header />,
+      <tbody>,
+        {newPlanets.map((planet) =>
+          <tr key={planet.name}>
+            <td>{planet.name}</td>
+            <td>{planet.population}</td>
+            <td>{planet.rotation_period}</td>
+            <td>{planet.orbital_period}</td>
+            <td>{planet.diameter}</td>
+            <td>{planet.climate}</td>
+            <td>{planet.gravity}</td>
+            <td>{planet.terrain}</td>
+            <td>{planet.surface_water}</td>
+            <td>{planet.created}</td>
+            <td>{planet.edited}</td>
+          </tr>,
+        )}
       </tbody>
     </table>
   );
