c05478ed70f88f119b57ec7ede2a2994c2cb117f
djonata-dias <diasdjonata@gmail.com>
2020-07-29 21:43:26 -0300

    Criei uma funçao que quando chamada faz uma requisiçao a url passada como parametro

---
+++ src/utils/APIrequest.js
@@ -0,0 +1,10 @@
+const requestAPI = (url) =>
+  fetch(url).then((response) =>
+    response.json().then((json) => {
+      if (response.ok) return Promise.resolve(json);
+      return Promise.reject(json);
+    },
+    ),
+  );
+
+export default requestAPI;
