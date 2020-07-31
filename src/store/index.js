bd46a2c27401f9ecdc8509385fe08ddf5bae049b
gabriellukke <gabriellukke@outlook.com>
2020-07-31 03:19:23 -0300

    Requirement 1 - CC v1.0

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ import {
 import thunk from 'redux-thunk';
 import rootReducer from '../reducers/index';
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const composeEnhancers = /* window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ||  */compose;
 
 const store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
 
