a0bededfd3504689437478a503dc9c86a5046995
Thiago Tavares <thiagooo15@gmail.com>
2020-08-03 18:39:39 -0300

    vam?

--- src/components/Filter/FilterValues.jsx
+++ src/components/Filter/FilterValues.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState,useContext } from 'react'
 import StarWarsContext from '../../context/StarWarsContext';
 import { updateColumns, getColumns, getComparation } from './Selectors';
 
