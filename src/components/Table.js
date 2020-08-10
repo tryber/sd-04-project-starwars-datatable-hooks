298b47b4048f0119a1d4d4a8752ccca8f5d55411
wistyk <leonardoxw@gmail.com>
2020-08-10 15:40:48 -0300

    Requirement 1

---
+++ src/components/Table.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import TableBody from './TableBody';
+
+export default function Table() {
+  return (
+    <div>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Rotation Period</th>
+            <th>Orbital Period</th>
+            <th>Diameter</th>
+            <th>Climate</th>
+            <th>Gravity</th>
+            <th>Terrain</th>
+            <th>Surface Water</th>
+            <th>Population</th>
+            <th>Film</th>
+            <th>Created</th>
+            <th>Edited</th>
+            <th>URL</th>
+          </tr>
+        </thead>
+        <TableBody />
+      </table>
+    </div>
+  );
+}
---
+++ src/components/TableBody.js
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from 'react';
+import { getPlanets } from '../services/apis';
+
+export default function TableBody() {
+  const [planets, setPlanets] = useState([]);
+  useEffect(() => {
+    getPlanets().then((data) => setPlanets(data.results));
+  }, []);
+
+  return (
+    <tbody>
+      {planets.map((planet) => (
+        <tr key={planet.name}>
+          <td>{planet.name}</td>
+          <td>{planet.rotation_period}</td>
+          <td>{planet.orbital_period}</td>
+          <td>{planet.diameter}</td>
+          <td>{planet.climate}</td>
+          <td>{planet.gravity}</td>
+          <td>{planet.terrain}</td>
+          <td>{planet.surface_water}</td>
+          <td>{planet.population}</td>
+          <td>
+            {planet.films.map((film) => (
+              <span key={film}>{film}</span>
+            ))}
+          </td>
+          <td>{planet.created}</td>
+          <td>{planet.edited}</td>
+          <td>{planet.url}</td>
+        </tr>
+      ))}
+    </tbody>
+  );
+}
