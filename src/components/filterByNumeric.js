168ae557e0e9a8f49a84147a5f99f349cd5e6245
Julio Cesar <stininmatic@hotmail.com>
2020-08-12 15:09:40 -0300

    Requisito 2 touppercase

--- src/components/filterByNumeric.js
+++ src/components/filterByNumeric.js
@@ -5,23 +5,21 @@ const filterByNumeric = () => (
   // const { data } = useContext(AppContext);
   // const { filterByNumericValues } = data;
 
-  return (
-    <div>
-      <select data-testid="column-filter">
-        <option value="population">population</option>
-        <option value="orbital_period">orbital_period</option>
-        <option value="diam eter">diameter</option>
-        <option value="rotation_period">rotation_period</option>
-        <option value="surface_water">surface_water</option>
-      </select>
+  <div>
+    <select data-testid="column-filter">
+      <option value="population">population</option>
+      <option value="orbital_period">orbital_period</option>
+      <option value="diam eter">diameter</option>
+      <option value="rotation_period">rotation_period</option>
+      <option value="surface_water">surface_water</option>
+    </select>
 
-      <select data-testid="comparison-filter">
-        <option value="maior que">maior que</option>
-        <option value="menor que">menor que</option>
-        <option value="igual a">igual a</option>
-      </select>
-    </div>
-  );
+    <select data-testid="comparison-filter">
+      <option value="maior que">maior que</option>
+      <option value="menor que">menor que</option>
+      <option value="igual a">igual a</option>
+    </select>
+  </div>
 );
 
 export default filterByNumeric;
