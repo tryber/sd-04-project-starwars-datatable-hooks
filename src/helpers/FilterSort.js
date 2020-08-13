7f3b2414efb58d5c718d14675e7343a2cb5bcd84
Tiago-ers <trsilva284@gmail.com>
2020-08-13 13:52:41 -0300

    refatorando filtro 2

--- src/helpers/FilterSort.js
+++ src/helpers/FilterSort.js
@@ -7,33 +7,33 @@ const numericKeys = [
   'population',
 ];
 
+/*
+    A função sort recebe dois parametros.
+    Se retornar -1 indica que a vêm antes de b.
+    Se retornar 1 indica que b vêm antes de a.
+    Se retornar 0 são iguais, não faz nada.
+    Referência: https://www.smashingmagazine.com/2020/03/sortable-tables-react/
+    */
+
 // Ordenando colunas
 const orderTable = (filtered, filters) => {
-
   // Verifica se a coluna é numerica
   if (numericKeys.includes(filters.order.column)) {
     if (filters.order.sort === 'ASC') {
       return filtered.sort(
         (a, b) =>
-          Number(a[filters.order.column]) - Number(b[filters.order.column]),
+          Number(a[filters.order.column]) - Number(b[filters.order.column])
       );
     }
     if (filters.order.sort === 'DESC') {
       return filtered.sort(
         (a, b) =>
-          Number(b[filters.order.column]) - Number(a[filters.order.column]),
+          Number(b[filters.order.column]) - Number(a[filters.order.column])
       );
     }
   }
 
-  /*
-      A função sort recebe dois parametros.
-      Se retornar -1 indica que a vêm antes de b.
-      Se retornar 1 indica que b vêm antes de a.
-      Se retornar 0 são iguais, não faz nada.
-      Referência: https://www.smashingmagazine.com/2020/03/sortable-tables-react/
-    */
-  filtered.sort((a, b) => {    
+  filtered.sort((a, b) => {
     if (
       a[filters.order.column.toLowerCase()] <
       b[filters.order.column.toLowerCase()]
