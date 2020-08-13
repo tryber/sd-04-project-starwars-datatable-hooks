71c4df408ab57d6b928ced8c4e6b440ac0e710d1
wistyk <leonardoxw@gmail.com>
2020-08-13 08:45:49 -0300

    maldito -am

---
+++ src/components/RemoveFilters.js
@@ -0,0 +1,20 @@
+import React, { useContext } from 'react';
+import { PlanetsContext } from '../context';
+
+export default function RemoveFilters() {
+  const { filterByNumericValues, setFilterByNumericValues } = useContext(
+    PlanetsContext
+  );
+  const onClick = (filterKeys) =>
+    setFilterByNumericValues(filterKeys, [
+      filterByNumericValues.filter((item) => item !== filterKeys),
+    ]);
+  return filterByNumericValues.map((item) => (
+    <div key={item.value} data-testid="filter">
+      <span>{`${item.column} - ${item.comparison} - ${item.value} `}</span>
+      <button type="button" onClick={() => onClick(item)}>
+        X
+      </button>
+    </div>
+  ));
+}
