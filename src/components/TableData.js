6e5299cecbe31bd0af8d5ec790115374ec506b73
Pedro <pedrotpoliveira@gmail.com>
2020-08-12 22:14:04 -0300

    Starting from scratch (almost)

--- src/components/TableData.js
+++ src/components/TableData.js
@@ -1,39 +1,14 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React from 'react';
 
-export default class TableData extends Component {
-  renderHeader = data => {
-    if (data) {
-      return Object.keys(data).map((attr, i) => <th key={i}>{attr}</th>);
-    }
-    return <p>NOTHING FOUND</p>;
-  };
+const TableData = () => (
+  <table>
+    <thead>
+      <tr>
+        <th>Teste</th>
+      </tr>
+    </thead>
+    <tbody><tr><td>Teste</td></tr></tbody>
+  </table>
+);
 
-  renderTable = data => data.map((planet, i) => (
-    <tr key={i}>
-      {Object.values(planet).map((attr, y) => (
-        <td key={y}>{attr}</td>
-      ))}
-    </tr>
-  ));
-
-  render() {
-    const { results } = this.props;
-    return (
-      <table>
-        <thead>
-          <tr>{this.renderHeader(results[0])}</tr>
-        </thead>
-        <tbody>{this.renderTable(results)}</tbody>
-      </table>
-    );
-  }
-}
-
-TableData.propTypes = {
-  results: PropTypes.arrayOf(PropTypes.object),
-};
-
-TableData.defaultProps = {
-  results: null,
-};
+export default TableData;
--- src/service/index.js
+++ src/service/index.js
@@ -1,9 +1,19 @@
+import axios from 'axios';
+
+// export default function apiRequest(endpoint) {
+//   return fetch(
+//     `https://swapi-trybe.herokuapp.com/api/${endpoint}/`,
+//   ).then((response) => response
+//     .json()
+//     .then(
+//       (data) => (response.ok ? Promise.resolve(data) : Promise.reject(data)),
+//     ));
+// }
+
 export default function apiRequest(endpoint) {
-  return fetch(
-    `https://swapi-trybe.herokuapp.com/api/${endpoint}/`,
-  ).then(response => response
-    .json()
-    .then(
-      data => (response.ok ? Promise.resolve(data) : Promise.reject(data)),
-    ));
+  return axios
+    .get(
+      `https://swapi-trybe.herokuapp.com/api/${endpoint}/`,
+    ).then((response) => response.data.results)
+    .catch((error) => (error));
 }
