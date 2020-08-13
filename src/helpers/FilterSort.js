ea7a73670fcddc2cec11d9db1f21a5240293c632
Tiago-ers <trsilva284@gmail.com>
2020-08-13 14:07:44 -0300

    refatorando filtro 3

--- src/helpers/FilterSort.js
+++ src/helpers/FilterSort.js
@@ -17,39 +17,42 @@ const numericKeys = [
 
 // Ordenando colunas
 const orderTable = (filtered, filters) => {
+  let coluna = numericKeys.includes(filters.order.column);
   // Verifica se a coluna é numerica
-  if (numericKeys.includes(filters.order.column)) {
+  if (coluna) {
     if (filters.order.sort === 'ASC') {
       return filtered.sort(
         (a, b) =>
-          Number(a[filters.order.column]) - Number(b[filters.order.column])
+          Number(a[filters.order.column]) - Number(b[filters.order.column]),
       );
     }
     if (filters.order.sort === 'DESC') {
       return filtered.sort(
         (a, b) =>
-          Number(b[filters.order.column]) - Number(a[filters.order.column])
+          Number(b[filters.order.column]) - Number(a[filters.order.column]),
       );
     }
   }
+  // Verifica colunas não numéricas
+  if (!coluna) {
+    filtered.sort((a, b) => {
+      if (
+        a[filters.order.column.toLowerCase()] <
+        b[filters.order.column.toLowerCase()]
+      ) {
+        return -1;
+      }
 
-  filtered.sort((a, b) => {
-    if (
-      a[filters.order.column.toLowerCase()] <
-      b[filters.order.column.toLowerCase()]
-    ) {
-      return -1;
-    }
+      if (
+        a[filters.order.column.toLowerCase()] >
+        b[filters.order.column.toLowerCase()]
+      ) {
+        return 1;
+      }
 
-    if (
-      a[filters.order.column.toLowerCase()] >
-      b[filters.order.column.toLowerCase()]
-    ) {
-      return 1;
-    }
-
-    return 0;
-  });
+      return 0;
+    });
+  }
   return filters.order.sort === 'DESC' ? filtered.reverse() : filtered;
 };
 
