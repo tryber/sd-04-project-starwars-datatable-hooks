183e7e7fe7921766774a9344b729504a294ce3af
Tiago-ers <trsilva284@gmail.com>
2020-08-15 13:42:56 -0300

    Nova refatoraçao filtro

--- src/helpers/FilterSort.js
+++ src/helpers/FilterSort.js
@@ -1,63 +1,59 @@
-// Colunas numericas
-const numericKeys = (filters) => {
-  const numeric =[
-    'rotation_period',
-    'orbital_period',
-    'diameter',
-    'surface_water',
-    'population',
-  ];
-
-  return numeric.includes(filters.order.column);
-};
-
 /*
     A função sort recebe dois parametros.
     Se retornar -1 indica que a vêm antes de b.
     Se retornar 1 indica que b vêm antes de a.
     Se retornar 0 são iguais, não faz nada.
     Referência: https://www.smashingmagazine.com/2020/03/sortable-tables-react/
-    */
+*/
 
 // Ordenando colunas
-const orderTable = (filtered, filters) => {
-  const coluna = numericKeys(filters);
-  // Verifica se a coluna é numerica
-  if (coluna) {
-    if (filters.order.sort === 'ASC') {
-      return filtered.sort(
-        (a, b) =>
-          Number(a[filters.order.column]) - Number(b[filters.order.column]),
-      );
-    }
-    if (filters.order.sort === 'DESC') {
-      return filtered.sort(
-        (a, b) =>
-          Number(b[filters.order.column]) - Number(a[filters.order.column]),
-      );
-    }
-  }
-  // Verifica colunas não numéricas
-  if (!coluna) {
-    filtered.sort((a, b) => {
-      if (
-        a[filters.order.column.toLowerCase()] <
-        b[filters.order.column.toLowerCase()]
-      ) {
-        return -1;
+function orderTable(filtered, filters) {
+  // Faz a ordenação das colunas numéricas
+  switch (filters.order.column) {
+    case 'rotation_period':
+    case 'orbital_period':
+    case 'surface_water':
+    case 'population':
+    case 'diameter':
+      if (filters.order.sort === 'ASC') {
+        filtered.sort(
+          (a, b) =>
+            Number(a[filters.order.column]) - Number(b[filters.order.column]),
+        );
+        return filtered;
       }
 
-      if (
-        a[filters.order.column.toLowerCase()] >
-        b[filters.order.column.toLowerCase()]
-      ) {
-        return 1;
+      if (filters.order.sort === 'DESC') {
+        filtered.sort(
+          (a, b) =>
+            Number(b[filters.order.column]) - Number(a[filters.order.column]),
+        );
+        return filtered;
       }
+      break;
+    // As colunas não numericas serão ordenadas por default.
+    // Ascendente do menor para o maior.
+    default:
+      filtered.sort((a, b) => {
+        if (
+          a[filters.order.column.toLowerCase()] <
+          b[filters.order.column.toLowerCase()]
+        ) {
+          return -1;
+        }
 
-      return 0;
-    });
+        if (
+          a[filters.order.column.toLowerCase()] >
+          b[filters.order.column.toLowerCase()]
+        ) {
+          return 1;
+        }
+
+        return 0;
+      });
+      // Inverte a ordenção para Descendente, do maior para o menor.
+      return filters.order.sort === 'DESC' ? filtered.reverse() : filtered;
   }
-  return filters.order.sort === 'DESC' ? filtered.reverse() : filtered;
-};
+}
 
 export default orderTable;
