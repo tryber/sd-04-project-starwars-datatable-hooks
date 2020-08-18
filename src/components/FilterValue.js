9903086b8f4792c2cde653adecf658eb95c0b345
Kaecio <kaecio@yahoo.com.br>
2020-08-18 12:11:11 -0300

    Requisito filtrar valor numerico e nao repetir, CC

--- src/components/FilterValue.js
+++ src/components/FilterValue.js
@@ -3,24 +3,24 @@ import StarWarsContext from '../context/StarWarsContext';
 
 function columnsName() {
   const coluna = [
-    "",
-    "population",
-    "orbital_period",
-    "diameter",
-    "rotation_period",
-    "surface_water",
+    '',
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
   ];
   return coluna.map((el) => <option key={el}>{el}</option>);
 }
 function valorComparation() {
-  const coparation = ["", "maior que", "igual a", "menor que"];
+  const coparation = ['', 'maior que', 'igual a', 'menor que'];
   return coparation.map((el) => <option key={el}>{el}</option>);
 }
 
 const FilterValue = () => {
   const { setData } = useContext(StarWarsContext);
-  const [column, setColumn] = useState("");
-  const [comparison, setComparison] = useState("");
+  const [column, setColumn] = useState('');
+  const [comparison, setComparison] = useState('');
   const [value, setValue] = useState(0);
 
   function onClick() {
@@ -36,25 +36,25 @@ const FilterValue = () => {
   return (
     <div>
       <select
-        data-testid="column-filter"
-        type="text"
+        data-testid='column-filter'
+        type='text'
         onChange={(event) => setColumn(event.target.value)}
       >
         {columnsName()}
       </select>
       <select
-        data-testid="comparison-filter"
-        type="text"
+        data-testid='comparison-filter'
+        type='text'
         onChange={(event) => setComparison(event.target.value)}
       >
         {valorComparation()}
       </select>
       <input
-        data-testid="value-filter"
-        type="number"
+        data-testid='value-filter'
+        type='number'
         onChange={(event) => setValue(+event.target.value)}
       />
-      <button data-testid="button-filter" onClick={() => onClick()}>
+      <button data-testid='button-filter' onClick={() => onClick()}>
         Filtrar
       </button>
     </div>
