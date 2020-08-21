710542a9a67fe14c4d5e29dab7e26eded17facc2
lourenco <loulage@gmail.com>
2020-08-21 15:01:37 -0300

    Created starwars context

---
+++ src/context/SWcontext.js
@@ -0,0 +1,66 @@
+import React, { createContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const SWcontext = createContext();
+
+const StarWarsProvider = ({ children }) => {
+    const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState([]);
+  
+  const [error, setError] = useState('');
+  const [filterByName, setFilterByName] = useState({ name: '' });
+  const [filterByNumericValues, setFilterByNumericValues] = useState([]);
+  const [order, setOrder] = useState({ column: 'Name', sort: 'ASC' });
+  const [options] = useState([
+    'population',
+    'orbital_period',
+    'diameter',
+    'rotation_period',
+    'surface_water',
+  ]);
+
+  const handleInput = (value) => {
+    setFilterByName({
+      name: value,
+    });
+  };
+
+  const submitFilters = (column, sort) => {
+    setOrder({
+      column,
+      sort,
+    });
+  };
+
+  const removeFilter = (col) => {
+    setFilterByNumericValues([...filterByNumericValues.filter(({ column }) => column !== col)]);
+  };
+
+  const submitFilterData = (column, comparison, value) => {
+    setFilterByNumericValues([...filterByNumericValues, { column, comparison, value }]);
+  };
+  const context = {
+    data,
+    setData,
+    isLoading,
+    setIsLoading,
+    error,
+    setError,
+    filterByName,
+    handleInput,
+    filterByNumericValues,
+    submitFilterData,
+    order,
+    submitFilters,
+    options,
+    removeFilter,
+  };
+
+  return <SWcontext.Provider value={context}>{children}</SWcontext.Provider>;
+};
+
+StarWarsProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+export { SWcontext, StarWarsProvider };
