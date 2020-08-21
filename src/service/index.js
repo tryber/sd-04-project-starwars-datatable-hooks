c3921fa41ad0083eb15ae17a4111af01065b51c2
Pedro <pedrotpoliveira@gmail.com>
2020-08-21 01:27:40 -0300

    Switching AXIOS to Fetch (Jest Mock)

--- src/service/index.js
+++ src/service/index.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
+// import axios from 'axios';
 
-// export default function apiRequest(endpoint) {
-//   return fetch(
-//     `https://swapi-trybe.herokuapp.com/api/${endpoint}/`,
-//   ).then((response) => response
-//     .json()
-//     .then(
-//       (data) => (response.ok ? Promise.resolve(data) : Promise.reject(data)),
-//     ));
-// }
-
-export default (endpoint) => axios
-  .get(
+export default function apiRequest(endpoint) {
+  return fetch(
     `https://swapi-trybe.herokuapp.com/api/${endpoint}/`,
-  ).then((response) => response.data.results)
-  .catch((error) => (error));
+  ).then((response) => response
+    .json()
+    .then(
+      (data) => (response.ok ? Promise.resolve(data.results) : Promise.reject(data)),
+    ));
+}
+
+// export default (endpoint) => axios
+//   .get(
+//     `https://swapi-trybe.herokuapp.com/api/${endpoint}/`,
+//   ).then((response) => response.data.results)
+//   .catch((error) => (error));
