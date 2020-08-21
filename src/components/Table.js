4f0cfd59b04d3f927dc0c5722370e26c91178bea
lourenco <loulage@gmail.com>
2020-08-21 17:16:30 -0300

    Implemented filterByName filter input

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,14 +1,19 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import useData from '../context/useData';
+import { SWcontext } from '../context/SWcontext';
 
 const Table = () => {
   const { data } = useData();
+  const { filterByName, handleInput } = useContext(SWcontext);
   let planets = [...data];
   const keys = data.length >= 1 ? Object.keys(data[0]) : [];
   const tableHeader = keys.filter((key) => key !== 'residents');
+  const inputText = filterByName.name;
+  if (inputText !== '') planets = planets.filter((planet) => planet.name.toLowerCase().includes(inputText.toLowerCase()));
 
   return (
     <div>
+       <input data-testid="name-filter" type="text" onChange={(e) => handleInput(e.target.value)} />
       <table>
         <thead>
           <tr>
