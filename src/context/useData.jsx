4a7688ddff044792fffc960dfdff88d21077b0c4
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-08-24 08:27:22 -0300

    adicionando custom hook

---
+++ src/context/useData.jsx
@@ -0,0 +1,33 @@
+import React, { useContext, useEffect } from 'react';
+import { StarWarsContext } from '../context/StarWarsContext';
+import getAPI from '../service/getAPI';
+
+const useData = () => {
+  const { loading, setLoading } = useContext(
+    StarWarsContext,
+  );
+
+  useEffect(() => {
+    setLoading(true);
+    getAPI().then((json) => {
+      console.log(json.results);
+      setIsLoading(false);
+    },
+    (error) => {
+      console.log(error);
+      setIsLoading(false);
+    },
+  );
+}, []);
+  if (loading) {
+    return <p>Loading</p>;
+  }
+  return (
+    <div>
+      <h1>StarWars Datatable</h1>
+      {/* <Table /> */}
+    </div>
+  );
+};
+
+export default useData;
