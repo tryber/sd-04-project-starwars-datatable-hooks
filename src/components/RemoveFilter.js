0f96e427d77140c92dd29577252318a0b9b5e4c3
Josias Viveiro <josiasviveiro@gmail.com>
2020-08-23 21:01:42 -0300

    Codeclimate issues - 3

--- src/components/RemoveFilter.js
+++ src/components/RemoveFilter.js
@@ -5,15 +5,15 @@ const RemoveFilter = () => {
   const { filters, setFilters } = useContext(StarWarsContext);
   const { filterByNumericValues } = filters;
   const handleOnClick = (itemFilter) => {
-    filterByNumericValues.filter((item) => item !== itemFilter);
-    setFilters({ filterByNumericValues: filterByNumericValues });
+    const filtered = filterByNumericValues.filter((item) => item !== itemFilter);
+    setFilters({ ...filters, filterByNumericValues: filtered });
   };
   return (
     <div>
       Filtros utilizados:
       <div>
-        {filterByNumericValues.map((item, index) => (
-          <div key={`${item.value}-${index}`} data-testid="filter">
+        {filterByNumericValues.map((item) => (
+          <div key={`${item.value}`} data-testid="filter">
             <p>{`${item.column} - ${item.comparison} - ${item.value}`}</p>
             <button type="button" onClick={() => handleOnClick(item)}>
               X
