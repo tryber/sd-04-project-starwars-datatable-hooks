725e42c24b01cafcb7b145f2052ac773d753de1b
Hugo J. De Moro Jr <hugo.urano@gmail.com>
2020-08-24 11:48:08 -0300

    codeclimate fixes

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,25 +1,22 @@
 import React from 'react';
+
 const CreateTableHeader = () => (
   <tr>
     <th>star wars!</th>
   </tr>
 );
-const CreateTableBody = () => {
-  return (
+const CreateTableBody = () => 
     <tr>
       <td data-testid="planet-name">Planet</td>
     </tr>
-  );
-}
-const Table = () => {
-  return (
+
+const Table = () => 
     <div>
       <table>
         <thead>{CreateTableHeader()}</thead>
         <tbody>{CreateTableBody()}</tbody>
       </table>
     </div>
-  );
-}
+
 
 export default Table;
