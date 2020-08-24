8dd1e2f651db3cfd459f44b1489d7d93de5d4dda
Rafael Passos Barbosa <59584187+rafaelpbar@users.noreply.github.com>
2020-08-24 12:00:50 -0300

    Code Climate

--- src/context/StarWarsProvider.js
+++ src/context/StarWarsProvider.js
@@ -24,42 +24,33 @@ const generalFilter = {
 };
 
 const ProviderSW = ({ children }) => {
-
   const [data, setPlanet] = useState([]);
-
   const [isFetching, setIsFetching] = useState(true);
-
   const [filter, setFilter] = useState(generalFilter);
-
   const setFetching = () => {
     setIsFetching();
   };
-
   const changeFilterName = (name) => {
     setFilter({ ...filter, filterByName: { name } });
   };
-
   const changeFilterColumn = (column, comparison, value) => {
     setFilter({
       ...filter,
       filterByNumericValues: [...filter.filterByNumericValues, { column, comparison, value }],
     });
   };
-
   const changeOrder = (column, sort) => {
     setFilter({
       ...filter,
       order: { column, sort },
     });
   };
-
   const removeFilter = (key) => {
     setFilter({
       ...filter,
       filterByNumericValues: filter.filterByNumericValues.filter((filtro) => filtro.column !== key),
     });
   };
-
   const contextValue = {
     data,
     isFetching,
@@ -71,7 +62,6 @@ const ProviderSW = ({ children }) => {
     removeFilter,
     changeOrder,
   };
-
   return <StarWarsContext.Provider value={contextValue}>{children}</StarWarsContext.Provider>;
 };
 
